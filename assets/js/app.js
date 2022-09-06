69881d634d4f82ade980f1012dbcb5977efe8add
siarkonyar <siarkonyar@gmail.com>
2022-09-06 14:18:17 +0300

    added loader

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,10 @@
+
+window.addEventListener('load', fg_load);
+
+function fg_load() {
+    document.getElementById('loading').style.display = 'none'
+}
+
 var button = document.getElementById("navbar-toggler");
 var carousel = document.getElementById("home-carousel");
 
